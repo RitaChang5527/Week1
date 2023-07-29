d68b1bba7cb6bbd0365b6b9e989db4dc8b2aaba9
RitaChang5527 <109656848+RitaChang5527@users.noreply.github.com>
2023-07-29 21:29:25 +0800

    Add files via upload

--- Week4/app.py
+++ Week4/app.py
@@ -2,14 +2,15 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import redirect
-
+from flask import session
 
 app=Flask(
     __name__,
     static_folder="static",
     static_url_path="/static"
 )
-signed_in = False
+
+app.secret_key = "any string but secret"
 
 #*處理index
 @app.route("/", methods=["GET"])
@@ -19,15 +20,18 @@ def home():
 #*處理sign in
 @app.route("/signin", methods=["POST"])
 def signin():
-    global signed_in
-
     #* 取得html中資料
     username = request.form["username"]
     password = request.form["password"] 
+
+    session["signed_in"]=True
+    session["username"] = username
     #* 驗證身分
+    if username=="" or password == "":
+        return redirect("/error?message=帳號或密碼空白")
+    
     if username == "test" and password == "test":
         # *如果成功了就會到會員頁面
-        signed_in = True
         return redirect("/member")
     else:
         #* 如果不成功就會
@@ -37,27 +41,20 @@ def signin():
 
 @app.route("/member", methods=["GET"])
 def member():
-    if not signed_in:
+    if not session.get("signed_in"):
         return redirect("/")
-    #* 返回會員頁面
+    #username = session.get("username")
     return render_template("member.html")
 
 @app.route("/signout", methods=["GET"])
 def signout():
-    global signed_in
-    signed_in = False
+    session["signed_in"]=False
     return redirect("/")
 
 @app.route("/error", methods=["GET"])
 def error():
-    #* Get the error message from the URL query parameter
     message = request.args.get("message")
-    #* Return the Error Page with the error message
     return render_template("error.html", message=f"{message}")
-
-
-
-
 
 #*啟動網站伺服器
 app.run(port=3000)
